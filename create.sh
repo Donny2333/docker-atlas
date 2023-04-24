346938252802b78990bb10c91a34f2e8343efc1b
Donny <1021001390@qq.com>
2023-04-24 20:39:29 +0800

    update shell

--- create.sh
+++ create.sh
@@ -1,10 +1,10 @@
-docker rm -f java8
+docker rm -f atlas
 docker run -d \
 	-it \
 	-p 8983:8983 \
 	-p 61510:61510 \
 	-p 21000:21000 \
-	--name java8 \
+	--name atlas \
 	-v $(pwd)/home:/root \
 	--privileged=true \
 	openjdk:8 \
